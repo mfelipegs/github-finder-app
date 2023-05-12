4a7baae6584105376926929b748c87153cf5e2f2
mfelipegs <matheusg.dasilva7@gmail.com>
2023-05-12 17:37:13 -0300

    Feat: App modules css and Search module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import classes from "./App.module.css";
 function App() {
-
   return (
-    <div className="app">
+    <div className={classes.app}>
       <h1>Github Finder</h1>
       <Outlet />
     </div>
   );
 }
 
-export default App
+export default App;
