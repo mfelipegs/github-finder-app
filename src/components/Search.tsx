58f2f0fc80f0c5d6609856fbbfe13c010b140389
mfelipegs <matheusg.dasilva7@gmail.com>
2023-05-12 17:57:13 -0300

    Feat: Enter (React KeyboardEvent) search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,12 +3,16 @@ type SearchProps = {
 };
 
 import React from "react";
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import { BsSearch } from "react-icons/bs";
 import classes from "./Search.module.css";
 
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+    }
+  };
 
   return (
     <div className={classes.search}>
@@ -19,6 +23,7 @@ const Search = ({ loadUser }: SearchProps) => {
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
