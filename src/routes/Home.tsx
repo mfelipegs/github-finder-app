a745a59d5622f58b6c1dd369c68ab6ab31eaec7b
mfelipegs <matheusg.dasilva7@gmail.com>
2023-05-12 17:02:49 -0300

    Chore: Type User

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import Search from "../components/Search";
 
 const Home = () => {
+  const [user, setUser] = useState(null);
+  
   return (
     <div>
       <Search />
---
+++ src/types/user.ts
@@ -0,0 +1,7 @@
+export type UsersProps = {
+  avatar_url: string;
+  login: string;
+  location: string;
+  followers: number;
+  following: number;
+};
