38f2a6026887213ccb446a9850b52ae14f521c6d
mfelipegs <matheusg.dasilva7@gmail.com>
2023-05-12 16:57:13 -0300

    Routes for Home, Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="app">
       <h1>Github Finder</h1>
+      <Outlet />
     </div>
   );
 }
