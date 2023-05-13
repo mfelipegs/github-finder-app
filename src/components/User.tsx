f881be0b87ffefd038327dcc02b8f97315ab8337
mfelipegs <matheusg.dasilva7@gmail.com>
2023-05-12 23:52:40 -0300

    Chore: User component return

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { UserProps } from "../types/user";
+import { MdLocationPin } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const User = ({
   login,
@@ -8,9 +10,27 @@ const User = ({
   following,
   location,
 }: UserProps) => {
-  return <div>
-    <img src={avatar_url} alt={login} />
-  </div>;
+  return (
+    <div>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      <p>
+        <MdLocationPin />
+        <span>{location}</span>
+      </p>
+      <div>
+        <div>
+          <p>Seguidores:</p>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <p>Seguindo:</p>
+          <p>{following}</p>
+        </div>
+      </div>
+      <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
+    </div>
+  );
 };
 
 export default User;
